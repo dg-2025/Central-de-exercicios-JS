fb990b2c83af8b2f763c3c0f0a2c49d0ec4f2115
FIC <fic@info.local>
2025-04-07 21:56:50 -0300

    coisa boa

--- condicionais.js
+++ condicionais.js
@@ -1,7 +1,7 @@
 // Nível Básico
 
 // Verificar maioridade
-function exercicio01() {
+function Basico01() {
     let idade = prompt("digite sua idade: ")
     if (idade >= 18) {
         console.log("ja pode ser preso")
@@ -15,7 +15,7 @@ function exercicio01() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Verificar se um número é positivo ou negativo:
-function exercicio02() {
+function Basico02() {
     let numero = prompt("digite um numero")
     if (numero > 0) {
 
@@ -32,7 +32,7 @@ function exercicio02() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Aprovação em uma prova:
-function exervivio03() {
+function basico03() {
     let nota = prompt("digiti sua nota: ")
     if (nota < 0 || nota > 100) {
         console.log("nota invalida")
@@ -48,7 +48,7 @@ function exervivio03() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Verificar se um número é positivo, negativo ou zero:
-function exercicio04() {
+function Básico04() {
     let numero = prompt("digite um numero")
     if (numero > 0) {
 
@@ -68,7 +68,7 @@ function exercicio04() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Classificação de idade:
-function exercicio05() {
+function Básico05() {
     let idade = prompt("digite sua idade")
     console.log(idade)
     if (idade >= 0 && idade <= 12) {
@@ -85,7 +85,7 @@ function exercicio05() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Verificar se um número é par ou ímpar:
-function exercicio06() {
+function Básico06() {
     let numero = prompt("digite um numero")
     console.log(numero)
     if (numero % 2 == 0) {
@@ -101,10 +101,10 @@ function exercicio06() {
 // Nível Intermediário
 
 // Calculadora simples:
-function exercicio06() {
-    let  n1 = prompt ("fale um numero")
-    let  n2 = prompt ("fale outro numero")
-    let operador = prompt ("escolha uma operação")
+function Intermediario01() {
+    let n1 = prompt("fale um numero")
+    let n2 = prompt("fale outro numero")
+    let operador = prompt("escolha uma operação")
     if (operador == "+") {
         let soma = Number(n1) + Number(n2)
         alert(soma)
@@ -121,38 +121,94 @@ function exercicio06() {
         let divisao = Number(n1) / Number(n2)
         alert(divisao)
     }
-        }
-    
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-  function exercicio07 () {
+function intermediario02() {
     debugger
-    let  n1 = prompt ("fale um numero")
-    let  n2 = prompt ("fale outro numero")
-    let  n3 = prompt ("fale o ultimo numero")
+    let n1 = prompt("fale um numero")
+    let n2 = prompt("fale outro numero")
+    let n3 = prompt("fale o ultimo numero")
     if (n1 > n2 && n1 > n3) {
         alert(n1 + " é seu maior numero")
-    } 
+    }
     else if (n2 > n1 && n2 > n3) {
         alert(n2 + " é seu maior numero")
     }
     else if (n3 > n1 && n3 > n2) {
         alert(n3 + " é seu maior numero")
     }
-  }
+}
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-   function exercicio08 () {
+function intermediario03() {
 
-    let valor = prompt ("digite um valor!: ")
+    let valor = prompt("digite um valor!: ")
     if (valor >= 100) {
-         valor = (Number(valor) * 10)/100 - Number(valor)
+        valor = (Number(valor) * 10) / 100 - Number(valor)
         alert(valor)
     }
     else {
         alert(valor)
     }
 
-   }
-   exercicio08 ()
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function intermediario04() {
+
+    let usuario = prompt("digite seu usuario")
+    let senha = prompt("digite su senha")
+    if (usuario == "admin" && senha == "1234") {
+        alert("seja bem vindo: " + usuario)
+    }
+    else {
+        alert("conta invalida")
+    }
+}
+
+
+// ***********************************************************************************************************************************
+
+//Nível Avançado
+
+function Avancado01() {
+    debugger
+    let lado1 = prompt("insira o tamanho de um lado do tringolo")
+    let lado2 = prompt("insira o tamanho de outro lado do tringolo")
+    let lado3 = prompt("insira o tamanho de ultimo lado do tringolo")
+    if (lado1 == lado2 == lado3) {
+        alert("triângulo equilatero")
+    }
+    else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+        alert("triângulo isósceles")
+    }
+    else if (lado1 != lado2 != lado3) {
+        alert("triângulo escaleno")
+
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+function Avancado02() {
+    let nota = Number(prompt("insira sua nota"))
+    if (nota >= 90 && nota <=100) {
+        alert ("A")
+    }
+    else if (nota >= 80 && nota <=89) {
+        alert ("B")
+    }
+    else if  (nota >= 70 &&  nota <=79) {
+        alert ("C")
+    }
+    else if (nota >= 60 && nota <=69) {
+        alert ("D")
+    }
+    else if (nota <60 ) {
+        alert ("F")
+    }
+}
